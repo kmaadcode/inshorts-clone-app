d0fb09e07be7f66579ece644a1ee2dde1c810d83
kmaadcode <kmaadcode@gmail.com>
2022-04-22 11:51:52 +0200

    firsrt commit app.js on main branch

---
+++ components/InshortTabs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const InshortTabs = () => {
+  return (
+    <View>
+      <Text>InshortTab</Text>
+    </View>
+  );
+};
+
+export default InshortTabs;
